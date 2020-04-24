8ca0d564639e6160d53f2674ebd91a83f683569a
beathris <beathris.anjasari@gmail.com>
2020-04-24 15:16:19 +0800

    add database configuration, data migation and seed

---
+++ database/migrations/2020_04_17_104510_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement();
+            $table->string('nama');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('telpon');
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('Role')->insert([
+             'nama_role' => 'Admin',
+        ]);
+         DB::table('Role')->insert([
+              'nama_role' => 'Pasien',
+         ]);
+          DB::table('Role')->insert([
+              'nama_role' => 'Psikeater',
+         ]);
+    }
+}
